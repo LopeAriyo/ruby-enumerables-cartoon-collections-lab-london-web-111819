65935fd76b031e1216488284375ee05e61f17e69
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-08 18:59:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,16 +16,16 @@ def summon_captain_planet(boring_food)# code an argument here
 
   exciting_food = []
   new_food = ""
-  i = 0 
+  i = 0
  #uppercase each element
 
-  until i == boring_food.length do 
+  until i == boring_food.length do
 
     new_food = boring_food[i].sub(boring_food[i][0],boring_food[i][0].uppercase)
     exciting_food.push("#{new_food}!)
     
   end
-  
+
 
   #p new_array
 
