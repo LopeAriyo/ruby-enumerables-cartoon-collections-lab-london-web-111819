3538100b7c43dbaf7debca0d56fe8dd51387ebaf
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-08 18:59:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,9 +12,20 @@ def roll_call_dwarves (dwarves)# code an argument here
 
 end
 
-def summon_captain_planet(food)# code an argument here
-  #new_array = []
- #uppercase each elemcent
+def summon_captain_planet(boring_food)# code an argument here
+
+  exciting_food = []
+  new_food = ""
+  i = 0 
+ #uppercase each element
+
+  until i == boring_food.length do 
+
+    new_food = boring_food[i].sub(boring_food[i][0],boring_food[i][0].uppercase)
+    exciting_food.push("#{new_food}!)
+    
+  end
+  
 
   #p new_array
 
